31a5fce823aad5ed3ad65800077b3058618b731a
medmonds <medmonds@homeaway.com>
2018-03-01 18:59:17 -0800

    initial push

---
+++ HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            }
+            else 
+            WindowState = WindowState.Maximized;
+        }
+
+        private void EnableSignIn()
+        {
+            var usernameLength = username.Text.Length;
+            var passwordLength = password.Text.Length;
+            if (usernameLength > 10 && passwordLength > 10)
+            {
+                btnSignIn.IsEnabled = true;
+            }
+            else btnSignIn.IsEnabled = false;
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void username_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (username.Text.Length > 9) {
+                EnableSignIn();
+            }
+        }
+
+        private void password_TextChanged(object sender, TextChangedEventArgs e)
+
+        {
+            if (password.Text.Length > 9)
+            {
+                EnableSignIn();
+            }
+        }
+    }
+}
