3fa8b744eaadba089d9b5b533719e3fff7ad275e
medmonds <medmonds@homeaway.com>
2018-03-06 20:25:47 -0800

    committing work from Session3Exercises into Homework02

--- HelloWorld/HelloWorld/MainWindow.xaml.cs
+++ HelloWorld/HelloWorld/MainWindow.xaml.cs
@@ -20,11 +20,21 @@ namespace HelloWorld
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Models.User user = new Models.User();
+        private static int minLength = 8;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        public override void EndInit()
+        {
+            base.EndInit();
+            uxContainer.DataContext = user;
+        }
+
+        // Maximize window button click
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (this.WindowState == WindowState.Maximized)
@@ -35,47 +45,34 @@ namespace HelloWorld
             WindowState = WindowState.Maximized;
         }
 
+        // Sign in button click
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            // todo
+        }
+        // method to check to see whether SignIn button should be enabled
         private void EnableSignIn()
         {
             var usernameLength = username.Text.Length;
             var passwordLength = password.Text.Length;
-            if (usernameLength >= 10 && passwordLength >= 10)
+            if (usernameLength >= minLength && passwordLength >= minLength)
             {
                 btnSignIn.IsEnabled = true;
             }
             else btnSignIn.IsEnabled = false;
         }
 
-        private void btnSignIn_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        // username events
         private void username_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (username.Text.Length >= 9) {
                 EnableSignIn();
-            }
         }
-
-        private void password_TextChanged(object sender, TextChangedEventArgs e)
-
+        private void username_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (password.Text.Length >= 9)
-            {
-                EnableSignIn();
-            }
+            labelUsername.Visibility = Visibility.Hidden;
         }
         private void username_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (username.Text.Length < 10)
-            {
-                usernameAlert.Visibility = Visibility.Visible;
-                usernameAlert.Text = "Username must be at least 10 characters long";
-            } else
-            {
-                usernameAlert.Visibility = Visibility.Hidden;
-            }
 
             if (username.Text.Length == 0)
             {
@@ -85,11 +82,15 @@ namespace HelloWorld
 
         }
 
-        private void username_GotFocus(object sender, RoutedEventArgs e)
-        {
-            labelUsername.Visibility = Visibility.Hidden;
-        }
+        // password events
+        private void password_TextChanged(object sender, TextChangedEventArgs e)
 
+        {
+            if (password.Text.Length >= minLength - 1)
+            {
+                EnableSignIn();
+            }
+        }
         private void password_GotFocus(object sender, RoutedEventArgs e)
         {
             labelPassword.Visibility = Visibility.Hidden;
@@ -97,16 +98,6 @@ namespace HelloWorld
 
         private void password_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (password.Text.Length < 10)
-            {
-                passwordAlert.Visibility = Visibility.Visible;
-                passwordAlert.Text = "Password must be at least 10 characters long";
-            }
-            else
-            {
-                passwordAlert.Visibility = Visibility.Hidden;
-            }
-
             if (password.Text.Length == 0)
             {
                 labelPassword.Visibility = Visibility.Visible;
