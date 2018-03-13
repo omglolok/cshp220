995f26b17f40b73e3d8d9932c7d0c45c7be39f5d
medmonds <medmonds@homeaway.com>
2018-03-12 20:10:33 -0700

    saving progress

---
+++ Homework02/Homework02/Models/User.cs
@@ -0,0 +1,142 @@
+﻿using System.ComponentModel;
+namespace Homework02.Models
+{
+    class User : IDataErrorInfo, INotifyPropertyChanged
+    {
+        private string name = string.Empty;
+        private string password = string.Empty;
+        private string nameError;
+        private string passwordError;
+
+        public string NameError
+        {
+            get
+            {
+                return nameError;
+            }
+            set
+            {
+                if (nameError != value)
+                {
+                    nameError = value;
+                    OnPropertyChanged("NameError");
+                }
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        public string PasswordError
+        {
+            get
+            {
+                return passwordError;
+            }
+            set
+            {
+                if (passwordError != value)
+                {
+                    passwordError = value;
+                    OnPropertyChanged("PasswordError");
+                }
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged("Password");
+                }
+            }
+        }
+
+        // IDataErrorInfo interface
+        public string Error
+        {
+            get
+            {
+                return "This is implementation of Error";
+            }
+        }
+
+        // IDataErrorInfo interface
+        // Called when ValidatesOnDataErrors=True
+        public string this[string columnName]
+        {
+            get
+            {
+                NameError = "";
+                PasswordError = "";
+                switch (columnName)
+                {
+                    case "Name":
+                        {
+                            if (string.IsNullOrEmpty(Name))
+                            {
+                                NameError = "Name cannot be empty.";
+                            }
+                            if (Name.Length > 12)
+                            {
+                                NameError = "Name cannot be longer than 12 characters.";
+                            }
+                            if (Name.Length < 8 && !string.IsNullOrEmpty(Name))
+                            {
+                                NameError = "Name must be longer than 8 characters.";
+                            }
+                            return NameError;
+                        }
+                    case "Password":
+                        {
+                            if (string.IsNullOrEmpty(Password))
+                            {
+                                PasswordError = "Password cannot be empty.";
+                            }
+                            if (Password.Length > 12)
+                            {
+                                PasswordError = "Password cannot be longer than 12 characters.";
+                            }
+                            if (Password.Length < 8)
+                            {
+                                PasswordError = "Password must be longer than 8 characters.";
+                            }
+                            return PasswordError;
+                        }
+                    default: return "";
+
+                }
+            }
+        }
+
+        // INotifyPropertyChanged interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
